2acff1a53cff548a403e4246e259ed0e6d067b65
niklasfalk99 <niklasfalk99@gmail.com>
2025-02-26 14:36:21 +0000

    map elites v4 works 78%

--- _mgsm/search.py
+++ _mgsm/search.py
@@ -602,7 +602,7 @@ if __name__ == "__main__":
     parser.add_argument('--debug', action='store_true', default=True)
     parser.add_argument('--save_dir', type=str, default='results/')
     parser.add_argument('--expr_name', type=str, default="mgsm_gpt3.5_results")
-    parser.add_argument('--n_generation', type=int, default=5)
+    parser.add_argument('--n_generation', type=int, default=40)
     parser.add_argument('--debug_max', type=int, default=5)
     parser.add_argument('--max_agents', type=int, default=2)
 
