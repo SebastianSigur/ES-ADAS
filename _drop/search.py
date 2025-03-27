f6874d18cd6f4d21eaf39017139eff003462df1e
Sebastían Sigurðarson <sebastiansigur@gmail.com>
2025-03-27 23:18:59 +0000

    dump

--- _drop/search.py
+++ _drop/search.py
@@ -408,7 +408,7 @@ if __name__ == "__main__":
     parser.add_argument('--save_dir', type=str, default='results/')
     parser.add_argument('--expr_name', type=str, default="drop_gpt3.5_results")
     parser.add_argument('--n_generation', type=int, default=30)
-    parser.add_argument('--debug_max', type=int, default=3)
+    parser.add_argument('--debug_max', type=int, default=1)
     parser.add_argument('--max_agents', type=int, default=5)
 
     print('no bad agents')
