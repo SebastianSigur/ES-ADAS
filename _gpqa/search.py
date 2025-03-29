897e8964b703b6962e6115f1abf5945f8ea36055
Sebastían Sigurðarson <sebastiansigur@gmail.com>
2025-03-29 21:42:39 +0000

    dump

--- _gpqa/search.py
+++ _gpqa/search.py
@@ -4,7 +4,7 @@ import json
 import os
 from collections import namedtuple
 from concurrent.futures import ThreadPoolExecutor
-
+import re
 import backoff
 import numpy as np
 import openai
@@ -173,8 +173,11 @@ def search(args):
         archive = get_init_archive()
         start = 0
 
+
+    initial_archive = []
     for solution in archive:
         if 'fitness' in solution:
+            initial_archive.append(get_upper_bound(solution['fitness']))
             continue
 
         solution['generation'] = "initial"
@@ -188,7 +191,7 @@ def search(args):
 
         fitness_str = bootstrap_confidence_interval(acc_list)
         solution['fitness'] = fitness_str
-
+        initial_archive.append(get_upper_bound(fitness_str))
         # save results
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
         with open(file_path, 'w') as json_file:
@@ -243,6 +246,11 @@ def search(args):
             continue
 
         fitness_str = bootstrap_confidence_interval(acc_list)
+        if get_upper_bound(fitness_str) < min(initial_archive):
+            print(f"Skipping agent because it has a lower fitness than the minimum fitness in the archive")
+            n -= 1
+            continue
+        
         next_solution['fitness'] = fitness_str
         next_solution['generation'] = n + 1
 
@@ -412,6 +420,7 @@ if __name__ == "__main__":
     parser.add_argument('--n_generation', type=int, default=30)
     parser.add_argument('--debug_max', type=int, default=1)
 
+    print(f"no bad agent")
     args = parser.parse_args()
     # search
     SEARCHING_MODE = True
