0fd91ef1c569465bc212335c83a66f865256ca5c
Sebastían Sigurðarson <sebastiansigur@gmail.com>
2025-03-31 11:13:22 +0100

    dump

--- _gpqa/search.py
+++ _gpqa/search.py
@@ -419,6 +419,7 @@ if __name__ == "__main__":
     parser.add_argument('--expr_name', type=str, default="gpqa_gpt3.5_results")
     parser.add_argument('--n_generation', type=int, default=30)
     parser.add_argument('--debug_max', type=int, default=1)
+    parser.add_argument('--max_agents', type=int, default=5)
 
     print(f"no bad agent")
     args = parser.parse_args()
